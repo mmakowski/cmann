23e1ebff3e6e362183c33675c9e878a483328f08
Maciek Makowski <maciek.makowski@gmail.com>
2015-04-12 14:52:16 +0100

    fixed smoke test

--- src/test/java/com/mmakowski/cmann/smoke/CmAnnSmokeTest.java
+++ src/test/java/com/mmakowski/cmann/smoke/CmAnnSmokeTest.java
@@ -3,6 +3,7 @@ package com.mmakowski.cmann.smoke;
 import com.mmakowski.cmann.CmAnnApp;
 import com.mmakowski.cmann.testcategories.SmokeTest;
 import com.mmakowski.util.TimingOut;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -16,18 +17,22 @@ import java.time.Instant;
 
 @Category(SmokeTest.class)
 public final class CmAnnSmokeTest {
+    private static final Duration totalReadTimeout = Duration.ofSeconds(2);
+    private static final Duration outputTimeout = Duration.ofSeconds(1);
+    private static final Duration retryDelay = Duration.ofMillis(100);
+
     @Test
     public void reactsToCommandsFromCommandLine() throws IOException, InterruptedException {
-        final Duration lineReadTimeout = Duration.ofMillis(100);
         final Process process = startApp();
         try {
             final PrintWriter inputWriter = new PrintWriter(process.getOutputStream());
             final BufferedReader outputReader = new BufferedReader(new InputStreamReader((process.getInputStream())));
             inputWriter.println("Alice -> smoke test");
             inputWriter.println("Alice");
+            inputWriter.flush();
             eventually(() -> {
-                final String outputLine = TimingOut.execute((TimingOut.Block<String>) outputReader::readLine, lineReadTimeout);
-                Assert.assertEquals("smoke test", outputLine);
+                final String outputLine = TimingOut.execute((TimingOut.Block<String>) outputReader::readLine, totalReadTimeout);
+                Assert.assertThat(outputLine, CoreMatchers.containsString("smoke test"));
             });
         } finally {
             process.destroyForcibly();
@@ -44,20 +49,18 @@ public final class CmAnnSmokeTest {
         return System.getProperty("java.class.path");
     }
 
-    private static final Duration TIMEOUT = Duration.ofSeconds(1);
-    private static final Duration RETRY_DELAY = Duration.ofMillis(100);
 
     private static void eventually(final Block block) throws InterruptedException {
         boolean succeeded = false;
         Throwable lastFailure = null;
         final Instant startTime = Instant.now();
-        while (!succeeded && Instant.now().isBefore(startTime.plus(TIMEOUT))) {
+        while (!succeeded && Instant.now().isBefore(startTime.plus(outputTimeout))) {
             try {
                 block.execute();
                 succeeded = true;
             } catch (final Throwable t) {
                 lastFailure = t;
-                Thread.sleep(RETRY_DELAY.toMillis());
+                Thread.sleep(retryDelay.toMillis());
             }
         }
         if (!succeeded) {
