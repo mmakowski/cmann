4c9c25b3085ca2b84a5cb7bac07601aeb390feca
Maciek Makowski <maciek.makowski@gmail.com>
2015-04-12 09:40:11 +0100

    output writing sink

--- src/main/java/com/mmakowski/cmann/text/OutputWritingResultSink.java
+++ src/main/java/com/mmakowski/cmann/text/OutputWritingResultSink.java
@@ -1,14 +1,20 @@
 package com.mmakowski.cmann.text;
 
 import com.mmakowski.cmann.exec.ResultSink;
+import com.mmakowski.cmann.model.Message;
 import com.mmakowski.cmann.model.Result;
 
 public final class OutputWritingResultSink implements ResultSink {
+    private final OutputWriter out;
+    private final MessageFormat messageFormat;
+
     public OutputWritingResultSink(final OutputWriter out, final MessageFormat messageFormat) {
+        this.out = out;
+        this.messageFormat = messageFormat;
     }
 
     @Override
     public void receive(final Result result) {
-
+        for (final Message message : result.messages) out.writeLine(messageFormat.format(message));
     }
 }
---
+++ src/test/java/com/mmakowski/cmann/text/OutputWritingResultSinkTest.java
@@ -0,0 +1,33 @@
+package com.mmakowski.cmann.text;
+
+import com.mmakowski.cmann.model.Message;
+import com.mmakowski.cmann.model.Result;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.Instant;
+
+public final class OutputWritingResultSinkTest {
+    @Test
+    public void writesAllFormattedMessagesToOutput() {
+        final Message message1 = new Message("Alice", "First message", Instant.parse("2015-04-12T09:15:30Z"));
+        final Message message2 = new Message("Bob", "Second message", Instant.parse("2015-04-12T10:00:30Z"));
+        final String formattedMessage1 = "Alice - First message (2 minutes)";
+        final String formattedMessage2 = "Bob - Second message (1 minute)";
+
+        final MessageFormat format = Mockito.mock(MessageFormat.class);
+        Mockito.when(format.format(message1)).thenReturn(formattedMessage1);
+        Mockito.when(format.format(message2)).thenReturn(formattedMessage2);
+
+        final OutputWriter out = Mockito.mock(OutputWriter.class);
+
+        final OutputWritingResultSink sink = new OutputWritingResultSink(out, format);
+        final Result result = Result.withMessages(message1, message2);
+
+        sink.receive(result);
+
+        Mockito.verify(out, Mockito.times(1)).writeLine(formattedMessage1);
+        Mockito.verify(out, Mockito.times(1)).writeLine(formattedMessage2);
+        Mockito.verifyNoMoreInteractions(out);
+    }
+}
